0cfdb550b4642281af57fe31849691b02b8e82b7
ZPyrolink <73246085+ZPyrolink@users.noreply.github.com>
2021-06-25 13:16:40 +0200

    Update ArgumentManager.cs
    Added new CheckHelp methods

--- ProgramArgumentsManager/ArgumentsManager.cs
+++ ProgramArgumentsManager/ArgumentsManager.cs
@@ -1,222 +1,234 @@
-﻿using System;
+﻿using ProgramArgumentsManager.Exceptions;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace ProgramArgumentsManager
 {
-    using Exceptions;
+	public class ArgumentsManager
+	{
+		private const int _DEFAULT_PAD_LEFT = 20;
 
-    public class ArgumentsManager
-    {
-        private const int _DEFAULT_PAD_LEFT = 20;
+		public string AppName { get; }
+		public string AppDescription { get; }
+		public string Version { get; }
 
-        public string AppName { get; }
-        public string AppDescription { get; }
-        public string Version { get; }
+		public string FullName => AppName + " " + Version;
 
-        public string FullName => AppName + " " + Version;
+		private readonly Dictionary<Argument, Argument.ArgValues> _options;
 
-        private readonly Dictionary<Argument, Argument.ArgValues> _options;
+		public ArgumentsManager(string name) : this(name, "1.0.0") { }
 
-        public ArgumentsManager(string name) : this(name, "1.0.0") { }
+		public ArgumentsManager(string name, string version) : this(name, version, null) { }
 
-        public ArgumentsManager(string name, string version) : this(name, version, null) { }
+		public ArgumentsManager(string name, string version, string description)
+		{
+			AppName = name;
+			Version = version;
+			AppDescription = description;
 
-        public ArgumentsManager(string name, string version, string description)
-        {
-            AppName = name;
-            Version = version;
-            AppDescription = description;
+			_options = new Dictionary<Argument, Argument.ArgValues>();
+		}
 
-            _options = new Dictionary<Argument, Argument.ArgValues>();
-        }
+		private static bool IsArgument(string s) => s.StartsWith("-") || s.StartsWith("--");
 
-        private static bool IsArgument(string s) => s.StartsWith("-") || s.StartsWith("--");
+		public void AddArguments(string format, string description)
+		{
+			format = format.Replace(" ", string.Empty);
 
-        public void AddArguments(string format, string description)
-        {
-            format = format.Replace(" ", string.Empty);
+			if (!format.Contains("-") && !format.Contains("--"))
+				throw new ArgumentException("Le format '" + format + " n'est pas comaptible en tant que paramètre !",
+					nameof(format));
 
-            if (!format.Contains("-") && !format.Contains("--"))
-                throw new ArgumentException("Le format '" + format + " n'est pas comaptible en tant que paramètre !",
-                    nameof(format));
+			char[] delimiters = { ',', ';' };
+			string[] formats = format.Split(delimiters);
 
-            char[] delimiters = {',', ';'};
-            string[] formats = format.Split(delimiters);
+			if (formats.Any(f => !f.StartsWith("-") && !f.StartsWith("--")))
+				throw new ArgumentException("Le format '" + format + " n'est pas comaptible en tant que paramètre !",
+					nameof(format));
 
-            if (formats.Any(f => !f.StartsWith("-") && !f.StartsWith("--")))
-                throw new ArgumentException("Le format '" + format + " n'est pas comaptible en tant que paramètre !",
-                    nameof(format));
+			AddArguments(new Argument(formats, description));
+		}
 
-            AddArguments(new Argument(formats, description));
-        }
+		private void AddArguments(Argument arg) => _options.Add(arg, new Argument.ArgValues());
 
-        private void AddArguments(Argument arg) => _options.Add(arg, new Argument.ArgValues());
+		public void AddHelpArgument() => AddArguments("-?, --help", "Show this help page");
 
-        public void AddHelpArgument() => AddArguments("-?, --help", "Show this help page");
-
-        public bool CheckHelp() => IsSpecified("?");
+		public bool CheckHelp() => IsSpecified("?");
 
 		public void CheckHelp<T>(Action callback)
 		{
-            if (IsSpecified("?"))
-                callback();
+			if (IsSpecified("?"))
+				callback();
 		}
 
-        public void CheckHelp<T1>(Action<T1> callback, T1 arg1)
+		public void CheckHelp<T1>(Action<T1> callback, T1 arg1)
 		{
-            if (IsSpecified("?"))
-                callback(arg1);
-        }
+			if (IsSpecified("?"))
+				callback(arg1);
+		}
 
-        public void CheckHelp<T1, T2>(Action<T1, T2> callback, T1 arg1, T2 arg2)
-        {
-            if (IsSpecified("?"))
-                callback(arg1, arg2);
-        }
+		public void CheckHelp<T1, T2>(Action<T1, T2> callback, T1 arg1, T2 arg2)
+		{
+			if (IsSpecified("?"))
+				callback(arg1, arg2);
+		}
 
-        public void CheckHelp<TResult, T1, T2>(Func<T1, T2, TResult> callback, T1 param1, T2 param2)
-        {
-            if (IsSpecified("?"))
-                callback(param1, param2);
-        }
+		public void CheckHelp<TResult>(Func<TResult> callback)
+		{
+			if (IsSpecified("?"))
+				callback();
+		}
 
-        public void AddVersionArgument() => AddArguments("-v, --version", "Show the app version");
+		public void CheckHelp<TResult, T1>(Func<T1, TResult> callback, T1 param1)
+		{
+			if (IsSpecified("?"))
+				callback(param1);
+		}
 
-        public bool CheckVersion() => IsSpecified("v");
+		public void CheckHelp<TResult, T1, T2>(Func<T1, T2, TResult> callback, T1 param1, T2 param2)
+		{
+			if (IsSpecified("?"))
+				callback(param1, param2);
+		}
 
-        public void Parse(string[] args)
-        {
-            string lastArg = null;
-            foreach (string arg in args)
-            {
-                if (IsArgument(arg))
-                {
-                    lastArg = arg;
-                    _options[lastArg].Specified = true;
-                }
-                else
-                {
-                    _options[lastArg].Values.Add(arg);
-                }
-            }
+		public void AddVersionArgument() => AddArguments("-v, --version", "Show the app version");
 
-            foreach (Argument.ArgValues argValues in _options.Values.Where(argValues => !argValues.Specified || !argValues.Values.Any()))
-                argValues.Values = null;
-        }
+		public bool CheckVersion() => IsSpecified("v");
 
-        public bool IsSpecified(string arg) => _options[arg].Specified;
+		public void Parse(string[] args)
+		{
+			string lastArg = null;
+			foreach (string arg in args)
+			{
+				if (IsArgument(arg))
+				{
+					lastArg = arg;
+					_options[lastArg].Specified = true;
+				}
+				else
+				{
+					_options[lastArg].Values.Add(arg);
+				}
+			}
 
-        public List<string> GetValues(string arg) => _options[arg].Values;
+			foreach (Argument.ArgValues argValues in _options.Values.Where(argValues => !argValues.Specified || !argValues.Values.Any()))
+				argValues.Values = null;
+		}
 
-        public string GetValue(string arg) => _options[arg].Values is null ? null : string.Join(" ", _options[arg].Values);
+		public bool IsSpecified(string arg) => _options[arg].Specified;
 
-        public bool HasValue(string arg) => _options[arg].Values != null;
+		public List<string> GetValues(string arg) => _options[arg].Values;
 
-        public void ShowUsage(int padLeft = _DEFAULT_PAD_LEFT) => Console.WriteLine(GetUsage(padLeft));
+		public string GetValue(string arg) => _options[arg].Values is null ? null : string.Join(" ", _options[arg].Values);
 
-        public string GetUsage(int padLeft = _DEFAULT_PAD_LEFT)
-        {
-            string usage = AppName + ":\n\n";
-            if (!(AppDescription is null))
-                usage += "\t" + AppDescription.Replace("\n", "\n\t") + "\n\n";
+		public bool HasValue(string arg) => _options[arg].Values != null;
 
-            usage += "USAGE: " + AppDomain.CurrentDomain.FriendlyName + "\n";
+		public void ShowUsage(int padLeft = _DEFAULT_PAD_LEFT) => Console.WriteLine(GetUsage(padLeft));
 
-            return _options.Aggregate(usage,
-                (current, option) =>
-                    current + option.Key.ToString().PadLeft(padLeft) + " = " + option.Key.Description + "\n");
-        }
+		public string GetUsage(int padLeft = _DEFAULT_PAD_LEFT)
+		{
+			string usage = AppName + ":\n\n";
+			if (!(AppDescription is null))
+				usage += "\t" + AppDescription.Replace("\n", "\n\t") + "\n\n";
 
-        public void ForceState(string arg, Argument.Ask ask) => _options.Keys.First(k => k.Names.Contains(arg)).Asked = ask;
+			usage += "USAGE: " + AppDomain.CurrentDomain.FriendlyName + "\n";
 
-        public void CheckRequired()
-        {
-            foreach (Argument argument in _options.Keys.Where(a => a.Asked == Argument.Ask.Required && !IsSpecified(a.Names[0])))
-                throw new ArgumentRequiredException(argument);
-        }
+			return _options.Aggregate(usage,
+				(current, option) =>
+					current + option.Key.ToString().PadLeft(padLeft) + " = " + option.Key.Description + "\n");
+		}
 
-        public List<Argument> GetMissingArguments() => GetMissingArguments(false);
-        public List<Argument> GetMissingArguments(bool permitNull)
-        {
-            Func<Argument, bool> predicate = a => permitNull ?
-                a.Asked == Argument.Ask.Required && !IsSpecified(a.Names[0]) :
-                !HasValue(a.Names[0]);
+		public void ForceState(string arg, Argument.Ask ask) => _options.Keys.First(k => k.Names.Contains(arg)).Asked = ask;
 
-            return _options.Keys.Where(predicate).ToList();
-        }
+		public void CheckRequired()
+		{
+			foreach (Argument argument in _options.Keys.Where(a => a.Asked == Argument.Ask.Required && !IsSpecified(a.Names[0])))
+				throw new ArgumentRequiredException(argument);
+		}
 
-        public class Argument
-        {
-            public enum Ask
-            {
-                Required,
-                Optional
-            }
+		public List<Argument> GetMissingArguments() => GetMissingArguments(false);
+		public List<Argument> GetMissingArguments(bool permitNull)
+		{
+			Func<Argument, bool> predicate = a => permitNull ?
+				a.Asked == Argument.Ask.Required && !IsSpecified(a.Names[0]) :
+				!HasValue(a.Names[0]);
 
-            public string[] Names { get; }
-            public string Description { get; }
-            public Ask Asked { get; internal set; }
+			return _options.Keys.Where(predicate).ToList();
+		}
 
-            internal Argument(string name, string desc) : this(new[] { name }, desc) { }
+		public class Argument
+		{
+			public enum Ask
+			{
+				Required,
+				Optional
+			}
 
-            internal Argument(string[] names, string desc)
-            {
-                Names = names;
-                Description = desc;
+			public string[] Names { get; }
+			public string Description { get; }
+			public Ask Asked { get; internal set; }
 
-                if (desc.StartsWith("[OPTIONAL]", StringComparison.InvariantCultureIgnoreCase))
-                    Asked = Ask.Optional;
-                else if (desc.StartsWith("[REQUIRED]", StringComparison.InvariantCultureIgnoreCase))
-                    Asked = Ask.Required;
-                else
-                    Asked = Ask.Optional;
-            }
+			internal Argument(string name, string desc) : this(new[] { name }, desc) { }
 
-            private bool Equals(Argument other) => other.Names.Any(s => Names.Contains(s) ||
-                Names.Contains("-" + s) || Names.Contains("--" + s));
+			internal Argument(string[] names, string desc)
+			{
+				Names = names;
+				Description = desc;
 
-            public override string ToString() => string.Join(", ", Names);
+				if (desc.StartsWith("[OPTIONAL]", StringComparison.InvariantCultureIgnoreCase))
+					Asked = Ask.Optional;
+				else if (desc.StartsWith("[REQUIRED]", StringComparison.InvariantCultureIgnoreCase))
+					Asked = Ask.Required;
+				else
+					Asked = Ask.Optional;
+			}
 
-            public override bool Equals(object obj)
-            {
-                if (obj is null)
-                    return false;
-                if (ReferenceEquals(this, obj))
-                    return true;
+			private bool Equals(Argument other) => other.Names.Any(s => Names.Contains(s) ||
+				Names.Contains("-" + s) || Names.Contains("--" + s));
 
-                return obj.GetType() == GetType() && Equals((Argument) obj);
-            }
+			public override string ToString() => string.Join(", ", Names);
 
-            public override int GetHashCode()
-            {
-                // Same HashCode for each instance to force the use of the Equals method (for the Dictionnary key)
-                return 0;
-            }
+			public override bool Equals(object obj)
+			{
+				if (obj is null)
+					return false;
+				if (ReferenceEquals(this, obj))
+					return true;
 
-            public static bool operator ==(Argument left, Argument right)
-            {
-                return Equals(left, right);
-            }
+				return obj.GetType() == GetType() && Equals((Argument) obj);
+			}
 
-            public static bool operator !=(Argument left, Argument right)
-            {
-                return !Equals(left, right);
-            }
+			public override int GetHashCode()
+			{
+				// Same HashCode for each instance to force the use of the Equals method (for the Dictionnary key)
+				return 0;
+			}
 
-            public static implicit operator Argument(string s) => new Argument(s, "Converted argument");
+			public static bool operator ==(Argument left, Argument right)
+			{
+				return Equals(left, right);
+			}
 
-            public class ArgValues
-            {
-                public List<string> Values { get; internal set; }
-                public bool Specified { get; internal set; }
+			public static bool operator !=(Argument left, Argument right)
+			{
+				return !Equals(left, right);
+			}
 
-                internal ArgValues()
-                {
-                    Values = new List<string>();
-                    Specified = false;
-                }
-            }
-        }
-    }
+			public static implicit operator Argument(string s) => new Argument(s, "Converted argument");
+
+			public class ArgValues
+			{
+				public List<string> Values { get; internal set; }
+				public bool Specified { get; internal set; }
+
+				internal ArgValues()
+				{
+					Values = new List<string>();
+					Specified = false;
+				}
+			}
+		}
+	}
 }
