496fd6b53528c18fc35aacbbfabdf667645e45de
ZPyrolink <73246085+ZPyrolink@users.noreply.github.com>
2021-06-25 14:47:17 +0200

    Exceptions
    Added FirstNoArgument and UnknownArgument Exceptions

---
+++ ProgramArgumentsManager/Exceptions/FirstNoArgumentException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ProgramArgumentsManager.Exceptions
+{
+	[Serializable]
+	public class FirstNoArgumentException : Exception
+	{
+		public FirstNoArgumentException() { }
+		public FirstNoArgumentException(string message) : base(message) { }
+		public FirstNoArgumentException(string message, Exception inner) : base(message, inner) { }
+		protected FirstNoArgumentException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+	}
+}
---
+++ ProgramArgumentsManager/Exceptions/UnknownArgumentException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ProgramArgumentsManager.Exceptions
+{
+	[Serializable]
+	public class UnknownArgumentException : Exception
+	{
+		public string Argument;
+
+		public UnknownArgumentException(string argument, string message) : base(message) { Argument = argument; }
+		protected UnknownArgumentException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+	}
+}
